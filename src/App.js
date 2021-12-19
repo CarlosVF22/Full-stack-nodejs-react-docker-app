3b6aee7fd967731e38bc90598b71369f357ebcc0
CarlosVF22 <cevasquezf22@gmail.com>
2021-12-19 13:53:30 -0500

    chronometer ready

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
+import PoolTable from './image/table.png';
+import Image from 'react-bootstrap/Image';
+import React from 'react';
+import {Chronometer} from './components/Chronometer';
+import { Container } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  
   return (
-    <p>Aca estoy</p>
+    <React.Fragment>
+      <Container>
+        <Image src={PoolTable} fluid={true}/>
+        <Chronometer/>
+      </Container>
+      <Container>
+        <Image src={PoolTable} fluid={true}/>
+        <Chronometer/>
+      </Container>
+    </React.Fragment>
+      
+
   );
 }
 
