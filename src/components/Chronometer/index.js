f821a0e2e0247f260fa21f85f954f986912801db
CarlosVF22 <cevasquezf22@gmail.com>
2021-12-19 13:54:27 -0500

    chronometer ready

---
+++ src/components/Chronometer/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import './chronometer.css';
+import Button from 'react-bootstrap/Button';
+import { Container } from "react-bootstrap";
+import { useTimer } from "../../hooks/useTimer";
+import { formatTime } from "../../utils/formatTime";
+
+function Chronometer() {
+    const {
+        timer, 
+        isActive, 
+        isPaused, 
+        handleStart, 
+        handlePause, 
+        handleReset
+    } = useTimer(0);
+
+    const time = formatTime(timer);
+
+    console.log(time);
+    console.log(typeof(time))
+
+    return (
+        <Container>
+            <h2>{time}</h2>
+            <Button
+                onClick={handleStart}
+                variant={'success'}
+            >
+                {'Comenzar'}
+            </Button>
+            <Button
+                onClick={handlePause}
+                variant={'danger'}
+            >
+                {'Pausar'}
+            </Button>
+            <Button
+                onClick={handleReset}
+                variant={'secondary'}
+            >
+                {'Reiniciar'}
+            </Button>
+        </Container>
+    );
+}
+
+export { Chronometer }
